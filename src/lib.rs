c71ea3ad8bbde2727ec3b9de2f2c6dbfc7666f75
kaoru <k@warpnine.io>
2023-10-17 11:56:17 +0900

    feat: format with typstfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
 mod compile;
 mod download;
 mod fonts;
+mod format;
 mod package;
 mod world;
 
 use std::time::Duration;
 
-pub use compile::CompileParams;
-
-pub fn compile(params: &CompileParams) -> Result<Duration, Box<dyn std::error::Error>> {
-    compile::compile(params)
-}
+pub use format::{format, FormatParams};
