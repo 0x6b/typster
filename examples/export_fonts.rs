a284783e443a812ae3b33118330fc3c81584ca74
kaoru <k@warpnine.io>
2023-12-09 17:50:48 +0900

    feat: return path to exported fonts

--- examples/export_fonts.rs
+++ examples/export_fonts.rs
@@ -2,5 +2,8 @@ use std::path::PathBuf;
 use std::str::FromStr;
 
 fn main() {
-    typster::export_fonts(&Vec::new(), &PathBuf::from_str("fonts").unwrap());
+    typster::export_fonts(&Vec::new(), &PathBuf::from_str("fonts").unwrap())
+        .unwrap()
+        .iter()
+        .for_each(|f| println!("{}", f.to_string_lossy()));
 }
