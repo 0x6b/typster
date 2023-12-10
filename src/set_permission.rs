e2bbd415adb9b50bb99fdecc9ceaab03235a6128
kaoru <k@warpnine.io>
2023-12-10 14:18:25 +0900

    feat: support PDF version 1.7 permission
    Since Typst generate PDF v1.7 document.

--- src/set_permission.rs
+++ src/set_permission.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, path::PathBuf};
 
-use qpdf::{EncryptionParams, EncryptionParamsR2};
+use qpdf::{EncryptionParams, EncryptionParamsR6};
 
 /// Parameters for permission.
 #[derive(Debug, Clone)]
@@ -12,28 +12,74 @@ pub struct PermissionParams {
     /// change.
     pub owner_password: String,
 
-    /// Allow extraction of text and images.
+    /// Allow content copying for accessibility.
+    pub allow_accessibility: bool,
+
+    /// Allow page extraction.
     pub allow_extract: bool,
 
+    /// Allow document assembly.
+    pub allow_assemble: bool,
+
+    /// Allow commenting and form filling.
+    pub allow_annotate_and_form: bool,
+
+    /// Allow form field fill-in or signing.
+    pub allow_form_filling: bool,
+
+    /// Allow other modifications.
+    pub allow_modify_other: bool,
+
     /// Allow printing.
-    pub allow_print: bool,
+    pub allow_print: PrintPermission,
 
-    /// Allow modification.
-    pub allow_modify: bool,
+    /// Encrypt metadata.
+    pub encrypt_metadata: bool,
+}
 
-    /// Allow annotation.
-    pub allow_annotate: bool,
+#[derive(Debug, Clone)]
+pub enum PrintPermission {
+    /// Allow printing in high resolution.
+    Full,
+    /// Allow printing only in low resolution.
+    Low,
+    /// Disallow printing.
+    None,
+}
+
+impl From<&PrintPermission> for qpdf::writer::PrintPermission {
+    fn from(permission: &PrintPermission) -> qpdf::writer::PrintPermission {
+        match permission {
+            PrintPermission::Full => qpdf::PrintPermission::Full,
+            PrintPermission::Low => qpdf::PrintPermission::Low,
+            PrintPermission::None => qpdf::PrintPermission::None,
+        }
+    }
+}
+
+impl From<String> for PrintPermission {
+    fn from(permission: String) -> PrintPermission {
+        match permission.to_lowercase().as_str() {
+            "full" => PrintPermission::Full,
+            "low" => PrintPermission::Low,
+            _ => PrintPermission::None,
+        }
+    }
 }
 
 impl From<&PermissionParams> for EncryptionParams {
     fn from(params: &PermissionParams) -> EncryptionParams {
-        EncryptionParams::R2(EncryptionParamsR2 {
+        EncryptionParams::R6(EncryptionParamsR6 {
             user_password: params.user_password.clone(),
             owner_password: params.owner_password.clone(),
+            allow_accessibility: params.allow_accessibility,
             allow_extract: params.allow_extract,
-            allow_print: params.allow_print,
-            allow_modify: params.allow_modify,
-            allow_annotate: params.allow_annotate,
+            allow_assemble: params.allow_assemble,
+            allow_annotate_and_form: params.allow_annotate_and_form,
+            allow_form_filling: params.allow_form_filling,
+            allow_modify_other: params.allow_modify_other,
+            allow_print: (&params.allow_print).into(),
+            encrypt_metadata: params.encrypt_metadata,
         })
     }
 }
@@ -43,10 +89,14 @@ impl Default for PermissionParams {
         Self {
             user_password: "".to_string(),
             owner_password: "".to_string(),
+            allow_accessibility: true,
             allow_extract: true,
-            allow_print: true,
-            allow_modify: false,
-            allow_annotate: true,
+            allow_assemble: false,
+            allow_annotate_and_form: true,
+            allow_form_filling: false,
+            allow_modify_other: false,
+            allow_print: PrintPermission::Full,
+            encrypt_metadata: true,
         }
     }
 }
