f8f1afe4b2314ca321f88ff223ce42cdb0e76805
kaoru <k@warpnine.io>
2024-02-07 23:51:29 +0900

    tests: add test for `version`

--- src/version.rs
+++ src/version.rs
@@ -1,2 +1,15 @@
 // See `build.rs` for how this file is generated.
 include!(concat!(env!("OUT_DIR"), "/version.rs"));
+
+#[cfg(test)]
+mod test {
+    use std::error::Error;
+
+    use crate::typst_version;
+
+    #[test]
+    fn test_version() -> Result<(), Box<dyn Error>> {
+        assert_eq!(typst_version(), "0.10.0");
+        Ok(())
+    }
+}
