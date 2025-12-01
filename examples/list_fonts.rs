93c91def49181e7bebb0e95f14256579f1875f3a
kaoru <k@warpnine.io>
2025-12-01 21:47:29 +0900

    chore: list font stretch information

--- examples/list_fonts.rs
+++ examples/list_fonts.rs
@@ -20,13 +20,21 @@ fn main() {
     list_fonts(&params.font_paths).iter().for_each(|(family, fontinfo)| {
         let mut sorted = fontinfo
             .iter()
-            .map(|info| (format!("{:?}", info.variant.style), format!("{:?}", info.variant.weight)))
+            .map(|info| {
+                (
+                    format!("{:?}", info.variant.style),
+                    format!("{:?}", info.variant.weight),
+                    format!("{:?}", info.variant.stretch),
+                )
+            })
             .collect::<Vec<_>>();
-        sorted.sort_by(|a, b| a.0.cmp(&b.0).then(a.1.cmp(&b.1)));
+        sorted.sort_by(|a, b| a.0.cmp(&b.0).then(a.1.cmp(&b.1)).then(a.2.cmp(&b.2)));
 
         println!("{}:", family);
         sorted
             .iter()
-            .for_each(|(style, weight)| println!("  - Style: {style}, Weight: {weight}"));
+            .for_each(|(style, weight, stretch)| {
+                println!("  - Style: {style}, Weight: {weight}, Stretch: {stretch}")
+            });
     });
 }
